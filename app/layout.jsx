a3a5cc204db7d59050f718bce98f9c74d89ad217
lynx-store <lynxstre22@gmail.com>
2026-09-13 02:48:38 +0300

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'LYNX Store',
+  description: 'LYNX E-Commerce Store',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ar" dir="rtl">
+      <body>{children}</body>
+    </html>
+  );
+}
